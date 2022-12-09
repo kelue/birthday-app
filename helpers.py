4a3eb99efb87f1bad79dcf2238201b8b31c1fc96
kelue <austincmoon5@gmail.com>
2022-12-09 04:38:42 +0100

    check for file format

--- helpers.py
+++ helpers.py
@@ -1,10 +1,18 @@
 from flask import redirect, render_template, session
 from functools import wraps
 
+# extensions for image files
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+
 def apology(template, message, code=500):
     """Render error on the same page with a message"""
 
     return render_template(template, error=message), code
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 def login_required(f):
