23570a04292ee96a82bbdfcdfbc5dba0dd2b148c
kelue <austincmoon5@gmail.com>
2022-12-10 18:35:17 +0100

    refactor code for copy to clipboard

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,10 +1,10 @@
-function copyText(){
+document.getElementById('copy-text').addEventListener('click', function(e){ 
     let myUrl = document.getElementById("user-link").innerHTML;
     myUrl = myUrl.split('<')
     myUrl = myUrl[0].trim()
     copytoclipboard(myUrl)
     alert(myUrl + ' copied to clipboard!')
-}
+});
 
 function copytoclipboard(str){
     const el = document.createElement('textarea');
